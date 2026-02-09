dda1d199a540124f10641703780c3c83a3bf3b67
hide card <hidecard1500@gmail.com>
2026-02-09 19:28:23 +0630

    add photo

--- constants.ts
+++ constants.ts
@@ -3,23 +3,23 @@ import { Memory } from './types';
 
 export const MEMORIES: Memory[] = [
   {
-    url: 'https://images.unsplash.com/photo-1518199266791-5375a83190b7?q=80&w=800&auto=format&fit=crop',
+    url: './love1.jpg',
     caption: 'ဒီအပြုံးလေးက ကို့ရဲ့ တစ်နေ့တာလုံးကို လင်းပွားစေတယ်'
   },
   {
-    url: 'https://images.unsplash.com/photo-1516589174184-c685266e430c?q=80&w=800&auto=format&fit=crop',
+    url: './love2.jpg',
     caption: 'ငယ်နဲ့ အတူစကားေပြာတဲ့ အချိန်တိုင်းက ကို့ရဲ့ အကြိုက်ဆုံး အမှတ်တရလေးတွေ'
   },
   {
-    url: 'https://images.unsplash.com/photo-1522673607200-1648832cee98?q=80&w=800&auto=format&fit=crop',
+    url: './love3.jpg',
     caption: 'ငယ်က ဘယ်အချိန်ကြည့်ကြည့် ပြည့်စုံလွန်းနေရောပဲ'
   },
   {
-    url: 'https://images.unsplash.com/photo-1511688868353-3a2f5be94cd7?q=80&w=800&auto=format&fit=crop',
+    url: './love4.jpg',
     caption: 'ငယ်က ကို့အတွက်တော့ အလှပဆုံးသော ပန်းကလေးတစ်ပွင့်ပါ'
   },
   {
-    url: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=800&auto=format&fit=crop',
+    url: './love5.jpg',
     caption: 'ကို့မျက်လုံးထဲမှာတော့ ငယ်က အရာရာထက် သာလွန်တယ်'
   }
 ];
